ca15a02508a0cd64bd110c2557d3f0761a0c44c4
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-11-18 13:03:25 +0000

    Test server error message solution

--- chalk_talk_api/settings.py
+++ chalk_talk_api/settings.py
@@ -61,7 +61,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-sophietiger-chalktalkap-fmr83iq5ivp.ws.codeinstitute-ide.net', "localhost",
     "127.0.0.1", os.environ.get('ALLOWED_HOST')]

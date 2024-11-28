2e935c37628ab32fa359be16c3bced49e0ebb8b8
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-11-28 10:10:00 +0000

    Change url pattern in personalrecords

--- personalrecords/urls.py
+++ personalrecords/urls.py
@@ -3,5 +3,5 @@ from personalrecords import views
 
 urlpatterns = [
     path('personalrecords/', views.PersonalRecordList.as_view()),
-    path('personalrecords/<int:pk>', views.PersonalRecordDetail.as_view()),
+    path('personalrecords/<int:pk>/', views.PersonalRecordDetail.as_view()),
 ]
